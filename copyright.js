181fbc4ea9e2b893d06b638e4f4051fed2f1070b
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-06-04 13:37:31 +0200

    Upd

--- copyright.js
+++ copyright.js
@@ -10,10 +10,12 @@
  */
 if (!window.AbrosCopyright) {
   window.AbrosCopyright = true;
-  setTimeout(() => {
-    document.head.insertAdjacentHTML(
-      "beforeend",
-      `
+  const observer = new MutationObserver((mutations, obs) => {
+    if (document.body) {
+      obs.disconnect();
+      document.head.insertAdjacentHTML(
+        "beforeend",
+        `
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');
           .color-transition {
@@ -21,70 +23,76 @@ if (!window.AbrosCopyright) {
           }
         </style>
       `
-    );
+      );
 
-    const container = document.createElement("div");
-    container.style.cssText =
-      "width:100vw;height:auto;margin:0;display:flex;justify-content:center;align-items:center;font-family:'Montserrat Alternates',sans-serif;background-color: black;padding: 2px;";
+      const container = document.createElement("div");
+      container.style.cssText =
+        "width:100vw;height:auto;margin:0;display:flex;justify-content:center;align-items:center;font-family:'Montserrat Alternates',sans-serif;background-color: black;padding: 2px;";
 
-    const link = document.createElement("a");
-    link.href = "https://abros.dev";
-    link.target = "_blank";
-    link.rel = "noopener";
-    link.style.cssText =
-      "display:flex;flex-wrap:wrap;justify-content:center;width:350px;text-decoration:none;color:white;";
+      const link = document.createElement("a");
+      link.href = "https://abros.dev";
+      link.target = "_blank";
+      link.rel = "noopener";
+      link.style.cssText =
+        "display:flex;flex-wrap:wrap;justify-content:center;width:350px;text-decoration:none;color:white;";
 
-    const title = document.createElement("p");
-    title.style.cssText =
-      "font-weight: bold;padding: 0 12px;border-radius: 2px;margin:0;font-size:small;";
-    title.textContent = "ABROS";
+      const title = document.createElement("p");
+      title.style.cssText =
+        "font-weight: bold;padding: 0 12px;border-radius: 2px;margin:0;font-size:small;";
+      title.textContent = "ABROS";
 
-    const description = document.createElement("p");
-    description.style.cssText =
-      "padding: 0 5px;border-radius: 2px;margin:0;font-size:xx-small;text-align:center;";
+      const description = document.createElement("p");
+      description.style.cssText =
+        "padding: 0 5px;border-radius: 2px;margin:0;font-size:xx-small;text-align:center;";
 
-    const translations = {
-      en: "The site or materials on the site are developed by developer Daniel Abros",
-      ru: "Сайт или материалы на сайте разработаны разработчиком Daniel Abros",
-      es: "El sitio o los materiales en el sitio están desarrollados por el desarrollador Daniel Abros",
-      fr: "Le site ou les matériaux sur le site sont développés par le développeur Daniel Abros",
-      de: "Die Website oder Materialien auf der Website wurden vom Entwickler Daniel Abros entwickelt",
-      pl: "Strona lub materiały na stronie są opracowane przez dewelopera Daniel Abros",
-    };
+      const translations = {
+        en: "The site or materials on the site are developed by developer Daniel Abros",
+        ru: "Сайт или материалы на сайте разработаны разработчиком Daniel Abros",
+        es: "El sitio o los materiales en el sitio están desarrollados por el desarrollador Daniel Abros",
+        fr: "Le site ou les matériaux sur le site sont développés par le développeur Daniel Abros",
+        de: "Die Website oder Materialien auf der Website wurden vom Entwickler Daniel Abros entwickelt",
+        pl: "Strona lub materiały na stronie są opracowane przez dewelopera Daniel Abros",
+      };
 
-    const userLang = navigator.language || navigator.userLanguage;
-    description.textContent = translations[userLang] || translations["en"];
-    console.log(
-      `${translations[userLang] || translations["en"]} | https://abros.dev`
-    );
+      const userLang = navigator.language || navigator.userLanguage;
+      description.textContent = translations[userLang] || translations["en"];
+      console.log(
+        `${translations[userLang] || translations["en"]} | https://abros.dev`
+      );
 
-    link.appendChild(title);
-    link.appendChild(description);
-    container.appendChild(link);
-    document.body.appendChild(container);
+      link.appendChild(title);
+      link.appendChild(description);
+      container.appendChild(link);
+      document.body.appendChild(container);
 
-    function getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+      function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
       }
-      return color;
-    }
 
-    function setRandomColor() {
-      const newColor = getRandomColor();
-      title.style.backgroundColor = `${newColor}80`;
-    }
+      function setRandomColor() {
+        const newColor = getRandomColor();
+        title.style.backgroundColor = `${newColor}80`;
+      }
 
-    function updateColor() {
-      title.style.transition = "background-color 1s, color 2s";
-      setRandomColor();
-      setTimeout(() => {
-        title.style.transition = "";
-      }, 1000);
-    }
+      function updateColor() {
+        title.style.transition = "background-color 1s, color 2s";
+        setRandomColor();
+        setTimeout(() => {
+          title.style.transition = "";
+        }, 1000);
+      }
 
-    setInterval(updateColor, 5000);
-  }, 10000);
+      setInterval(updateColor, 5000);
+    }
+  });
+
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
 }
