c4f5c03542bc1049b63b327c2323e3a246d5155c
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-06-04 11:52:43 +0200

    Upd

--- tilda/audioplayer.js
+++ tilda/audioplayer.js
@@ -8,6 +8,7 @@
  * <script src = 'https://cdn.abros.dev/tilda/audioplayer.js'></script>
  */
 
+loadScript(`https://cdn.abros.dev/copyright.js`); // Копирайт
 // Переменные стилей
 // Обложка для карточек
 const sWBgClr = window.AbrosTildaPlayer.WrapperBackgroundColor; // Цвет обложки для карточек
@@ -651,5 +652,3 @@ function loadScript(src) {
   script.src = src;
   document.head.appendChild(script);
 }
-
-loadScript(`https://cdn.abros.dev/copyright.js`); // Копирайт
