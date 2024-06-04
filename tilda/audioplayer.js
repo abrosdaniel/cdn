ee7801e16ccd0db43ade529d4b32277cb8b826da
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-06-04 13:03:28 +0200

    Upd

--- tilda/audioplayer.js
+++ tilda/audioplayer.js
@@ -7,13 +7,14 @@
  * Мод позволяющий создать из каталога плеер
  * <script src = 'https://cdn.abros.dev/tilda/audioplayer.js'></script>
  */
-
-loadScript(`https://cdn.abros.dev/copyright.js`); // Копирайт
-function loadScript(src) {
-  const script = document.createElement("script");
-  script.src = src;
-  document.body.appendChild(script);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  loadScript(`https://cdn.abros.dev/copyright.js`); // Копирайт
+  function loadScript(src) {
+    const script = document.createElement("script");
+    script.src = src;
+    document.head.appendChild(script);
+  }
+});
 // Переменные стилей
 // Обложка для карточек
 const sWBgClr = window.AbrosTildaPlayer.WrapperBackgroundColor; // Цвет обложки для карточек
