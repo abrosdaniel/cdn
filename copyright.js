07d568edf1602b17ba4e0039808552d4830a55ed
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-07-30 07:21:32 +0200

    Upd

--- copyright.js
+++ copyright.js
@@ -12,7 +12,7 @@
 if (!window.abros) {
   window.abros = {};
 
-  async function fetchData() {
+  const fetchData = async () => {
     try {
       const response = await fetch("https://cdn.abros.dev/copyright.json");
       return await response.json();
@@ -20,15 +20,13 @@ if (!window.abros) {
       console.error(error);
       return null;
     }
-  }
+  };
 
-  async function init() {
+  const init = async () => {
     const data = await fetchData();
     if (!data) return;
 
-    const translations = data.translations;
-    const sites = data.sites;
-
+    const { translations, sites } = data;
     const userLang = (navigator.language || navigator.userLanguage).split(
       "-"
     )[0];
@@ -39,20 +37,20 @@ if (!window.abros) {
 
     const hostname = window.location.hostname;
     const params = sites[hostname]?.copyright || {
-      type: "banner",
+      type: "footer",
       time: 10,
+      script: "none",
     };
 
     window.abros.translations = translations;
     window.abros.userLang = userLang;
 
-    if (params.type === "none") {
-      abros.initCanvas();
-    } else {
-      abros.initCopyright(params);
-      abros.initCanvas();
-    }
-  }
+    if (params.script !== "none") abros.loadScript(params.script);
+
+    if (params.type !== "none") abros.initCopyright(params);
+
+    abros.initCanvas();
+  };
 
   window.abros = {
     translations: null,
@@ -60,59 +58,41 @@ if (!window.abros) {
     domain: "https://abros.dev",
 
     initCopyright(params) {
-      if (params.type === "footer") {
-        this.addFooter(params.time);
-      }
-      if (params.type === "push") {
-        this.addPush(params.time);
-      }
-      if (params.type === "banner") {
-        this.addBanner(params.time);
+      switch (params.type) {
+        case "footer":
+          this.addFooter(params.time);
+          break;
+        case "push":
+          this.addPush(params.time);
+          break;
+        case "banner":
+          this.addBanner(params.time);
+          break;
       }
     },
 
     addFooter(time) {
       const { translations, userLang, domain } = this;
+      const container = this.createContainer();
+      const link = this.createLink(domain);
 
-      const container = document.createElement("div");
-      container.style.cssText =
-        "width:100vw;height:auto;margin:0;display:flex;justify-content:center;align-items:center;font-family:'Montserrat Alternates',sans-serif;background-color: black;padding: 2px;";
-
-      const link = document.createElement("a");
-      link.href = domain;
-      link.target = "_blank";
-      link.rel = "noopener";
-      link.style.cssText =
-        "display:flex;flex-wrap:wrap;justify-content:center;width:350px;text-decoration:none;color:white;";
-
-      const title = document.createElement("p");
-      title.style.cssText =
-        "font-weight: bold;padding: 0 12px;border-radius: 2px;margin:0;font-size:small; transition: background-color 1s, color 2s;";
-      title.textContent = "ABROS";
-
-      const description = document.createElement("p");
-      description.style.cssText =
-        "padding: 0 5px;border-radius: 2px;margin:0;font-size:xx-small;text-align:center;";
-      description.textContent = translations[userLang] || translations.en;
-
-      link.appendChild(title);
-      link.appendChild(description);
-      container.appendChild(link);
-
-      document.head.insertAdjacentHTML(
-        "beforeend",
-        `<style>
-          @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');
-        </style>`
+      const title = this.createTitle();
+      const description = this.createDescription(
+        translations[userLang] || translations.en
       );
 
-      setInterval(() => {
-        title.style.backgroundColor = `${this.getRandomColor()}80`;
-      }, 5000);
+      link.append(title, description);
+      container.appendChild(link);
 
-      setTimeout(() => {
-        document.documentElement.appendChild(container);
-      }, time * 1000);
+      document.head.insertAdjacentHTML("beforeend", this.getGoogleFont());
+      setInterval(
+        () => (title.style.backgroundColor = `${this.getRandomColor()}80`),
+        5000
+      );
+      setTimeout(
+        () => document.documentElement.appendChild(container),
+        time * 1000
+      );
     },
 
     addPush(time) {
@@ -124,9 +104,10 @@ if (!window.abros) {
 
       const text = translations[userLang] || translations.en;
       let noti = false;
+
       setInterval(() => {
         if (!noti) {
-          abrosnoti.create("abros", "ABROS", `${text}`, 0, true, () => {
+          abrosnoti.create("abros", "ABROS", text, 0, true, () => {
             window.open(domain, "_blank");
             noti = false;
           });
@@ -138,75 +119,29 @@ if (!window.abros) {
     addBanner(time) {
       const { translations, userLang, domain } = this;
 
-      const container = document.createElement("div");
-      container.style.cssText =
-        "width:110px;height:auto;margin:0;display:flex;justify-content:center;align-items:center;font-family:'Montserrat Alternates',sans-serif;background-color: black;padding: 2px;position: fixed;bottom: 50%;left: -100px;z-index: 999999999999999;transform: translateY(50%);border: 1px solid white;border-radius: 0 10px 10px 0;opacity: 0;transition: left 0.5s, opacity 1s;";
-      container.onmouseenter = () => {
-        container.style.left = "-10px";
-      };
-      container.onmouseleave = () => {
-        container.style.left = "-100px";
-      };
-      const stick = document.createElement("div");
-      stick.style.cssText =
-        "height: 90%;width: 4px;position: absolute;right: 6px;border-radius: 50px;overflow: hidden;";
+      const container = this.createBannerContainer();
+      const link = this.createLink(domain);
+      const title = this.createTitle();
+      const description = this.createDescription(
+        translations[userLang] || translations.en
+      );
+      const stick = this.createStick();
 
-      const stickColor = document.createElement("div");
-      stickColor.style.cssText =
-        "width: 10px;height: 40px;transform: translate(-50%, -50%);position: absolute;left: 50%;top: 0;transition: background-color 1s, color 2s;mask-image: radial-gradient(circle, rgba(0, 0, 0, 1) 30%, rgba(0, 0, 0, 0) 100%);-webkit-mask-image: radial-gradient(circle, rgba(0, 0, 0, 1) 30%, rgba(0, 0, 0, 0) 100%);animation: abroscopyright 3s cubic-bezier(0.65, 0, 0.29, 1) infinite alternate;";
+      link.append(title, description);
+      container.append(link, stick);
 
-      const link = document.createElement("a");
-      link.href = domain;
-      link.target = "_blank";
-      link.rel = "noopener";
-      link.style.cssText =
-        "display:flex;flex-wrap:wrap;justify-content:center;width:350px;text-decoration:none;color:white;";
-
-      const title = document.createElement("p");
-      title.style.cssText =
-        "font-weight: bold;padding: 0 12px;border-radius: 2px;margin:0;font-size:small; transition: background-color 1s, color 2s;";
-      title.textContent = "ABROS";
-
-      const description = document.createElement("p");
-      description.style.cssText =
-        "padding: 0 5px;border-radius: 2px;margin:0;font-size:xx-small;text-align:center;";
-      description.textContent = translations[userLang] || translations.en;
-
-      link.appendChild(title);
-      link.appendChild(description);
-      stick.appendChild(stickColor);
-      container.appendChild(link);
-      container.appendChild(stick);
       document.documentElement.appendChild(container);
-
       document.head.insertAdjacentHTML(
         "beforeend",
-        `<style>
-          @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');
-          @keyframes abroscopyright {
-            0% { top: 0; }
-            100% { top: 100%; }
-          }    
-        </style>`
+        this.getGoogleFont() + this.getBannerKeyframes()
       );
 
       setInterval(() => {
         title.style.backgroundColor = `${this.getRandomColor()}80`;
-        stickColor.style.backgroundColor = `${this.getRandomColor()}80`;
+        stick.firstChild.style.backgroundColor = `${this.getRandomColor()}80`;
       }, 5000);
 
-      setTimeout(() => {
-        container.style.opacity = "1";
-      }, time * 1000);
-    },
-
-    getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
+      setTimeout(() => (container.style.opacity = "1"), time * 1000);
     },
 
     initCanvas() {
@@ -218,21 +153,7 @@ if (!window.abros) {
       image.src = "https://cdn.abros.dev/abros.svg";
       let pressedKeys = "";
 
-      this.handleKeyDown = (event) => {
-        const keyPressed = event.key.toUpperCase();
-        pressedKeys += keyPressed;
-
-        // Проверяем последовательность
-        if (pressedKeys.includes("ABROS") && !animationStarted) {
-          if (!canvas) {
-            this.createCanvas();
-          }
-          this.launchFirework();
-          pressedKeys = "";
-        }
-      };
-
-      this.createCanvas = () => {
+      const createCanvas = () => {
         canvas = document.createElement("canvas");
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -245,15 +166,7 @@ if (!window.abros) {
         ctx = canvas.getContext("2d");
       };
 
-      this.launchFirework = () => {
-        animationStarted = true;
-        for (let i = 0; i < 100; i++) {
-          particles.push(this.createParticle());
-        }
-        this.animateFirework();
-      };
-
-      this.createParticle = () => {
+      const createParticle = () => {
         const x = Math.random() * canvas.width;
         const y = canvas.height;
         const speed = { x: (Math.random() - 0.5) * 8, y: Math.random() * -6 };
@@ -261,7 +174,13 @@ if (!window.abros) {
         return { x, y, speed, size };
       };
 
-      this.animateFirework = () => {
+      const launchFirework = () => {
+        animationStarted = true;
+        for (let i = 0; i < 100; i++) particles.push(createParticle());
+        animateFirework();
+      };
+
+      const animateFirework = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         particles.forEach((particle, index) => {
           particle.x += particle.speed.x;
@@ -274,19 +193,96 @@ if (!window.abros) {
             particle.size,
             particle.size
           );
-          if (particle.size <= 0 || particle.y > canvas.height) {
+          if (particle.size <= 0 || particle.y > canvas.height)
             particles.splice(index, 1);
-          }
         });
-        if (particles.length > 0) {
-          requestAnimationFrame(this.animateFirework);
-        } else {
-          animationStarted = false;
+        if (particles.length > 0) requestAnimationFrame(animateFirework);
+        else animationStarted = false;
+      };
+
+      const handleKeyDown = (event) => {
+        pressedKeys += event.key.toUpperCase();
+        if (pressedKeys.includes("ABROS") && !animationStarted) {
+          if (!canvas) createCanvas();
+          launchFirework();
+          pressedKeys = "";
         }
       };
-      document.addEventListener("keydown", (event) =>
-        this.handleKeyDown(event)
-      );
+
+      document.addEventListener("keydown", handleKeyDown);
+    },
+
+    getRandomColor() {
+      return `#${Math.floor(Math.random() * 0xffffff)
+        .toString(16)
+        .padEnd(6, "0")}`;
+    },
+
+    loadScript(src) {
+      const script = document.createElement("script");
+      script.src = src;
+      document.head.appendChild(script);
+    },
+
+    createContainer() {
+      const container = document.createElement("div");
+      container.style.cssText =
+        "width:100vw;height:auto;margin:0;display:flex;justify-content:center;align-items:center;font-family:'Montserrat Alternates',sans-serif;background-color: black;padding: 2px;";
+      return container;
+    },
+
+    createLink(domain) {
+      const link = document.createElement("a");
+      link.href = domain;
+      link.target = "_blank";
+      link.rel = "noopener";
+      link.style.cssText =
+        "display:flex;flex-wrap:wrap;justify-content:center;width:350px;text-decoration:none;color:white;";
+      return link;
+    },
+
+    createTitle() {
+      const title = document.createElement("p");
+      title.style.cssText =
+        "font-weight: bold;padding: 0 12px;border-radius: 2px;margin:0;font-size:small; transition: background-color 1s, color 2s;";
+      title.textContent = "ABROS";
+      return title;
+    },
+
+    createDescription(text) {
+      const description = document.createElement("p");
+      description.style.cssText =
+        "padding: 0 5px;border-radius: 2px;margin:0;font-size:xx-small;text-align:center;";
+      description.textContent = text;
+      return description;
+    },
+
+    createBannerContainer() {
+      const container = document.createElement("div");
+      container.style.cssText =
+        "width:110px;height:auto;margin:0;display:flex;justify-content:center;align-items:center;font-family:'Montserrat Alternates',sans-serif;background-color: black;padding: 2px;position: fixed;bottom: 50%;left: -100px;z-index: 999999999999999;transform: translateY(50%);border: 1px solid white;border-radius: 0 10px 10px 0;opacity: 0;transition: left 0.5s, opacity 1s;";
+      container.onmouseenter = () => (container.style.left = "-10px");
+      container.onmouseleave = () => (container.style.left = "-100px");
+      return container;
+    },
+
+    createStick() {
+      const stick = document.createElement("div");
+      stick.style.cssText =
+        "height: 90%;width: 4px;position: absolute;right: 6px;border-radius: 50px;overflow: hidden;";
+      const stickColor = document.createElement("div");
+      stickColor.style.cssText =
+        "width: 10px;height: 40px;transform: translate(-50%, -50%);position: absolute;left: 50%;top: 0;transition: background-color 1s, color 2s;mask-image: radial-gradient(circle, rgba(0, 0, 0, 1) 30%, rgba(0, 0, 0, 0) 100%);-webkit-mask-image: radial-gradient(circle, rgba(0, 0, 0, 1) 30%, rgba(0, 0, 0, 0) 100%);animation: abroscopyright 3s cubic-bezier(0.65, 0, 0.29, 1) infinite alternate;";
+      stick.appendChild(stickColor);
+      return stick;
+    },
+
+    getGoogleFont() {
+      return `<style>@import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');</style>`;
+    },
+
+    getBannerKeyframes() {
+      return `<style>@keyframes abroscopyright { 0% { top: 0; } 100% { top: 100%; } }</style>`;
     },
   };
 
