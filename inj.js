41acff9adcd0ef1c66190a202b61322bc65b416f
abrosxd <154523512+abrosxd@users.noreply.github.com>
2024-08-07 18:09:55 +0200

    Upd

--- inj.js
+++ inj.js
@@ -1,29 +1,20 @@
-/*!
- * Injector.js v1.0.0a
- * (c) 2024-2024
- * by Daniel Abros
- * Site → https://abros.dev
- * Telegram → https://t.me/abrosxd
- * Инжектор для подключения сторонних скриптов одной строкой
- * Используется для быстрой разработки
- * <script src = 'https://cdn.abros.dev/inj.js'></script>
- * <script type="module" src = 'https://cdn.abros.dev/inj.js'></script>
- */
+document.addEventListener("DOMContentLoaded", function () {
+  const block = document.querySelector(".t898");
+  const wrapper = document.querySelector(".t898__wrapper");
 
-if (!window.AbrosInj) {
-  (function () {
-    "use strict";
+  if (wrapper) {
+    wrapper.addEventListener("mouseenter", function () {
+      const randomBottom =
+        Math.floor(
+          Math.random() * (window.innerHeight - wrapper.offsetHeight)
+        ) + "px";
+      const randomRight =
+        Math.floor(Math.random() * (window.innerWidth - wrapper.offsetWidth)) +
+        "px";
 
-    /* Подключение плагинов */
-    loadScript(`https://cdn.abros.dev/noti/noti.js`); // Noti (Уведомления)
-    loadScript(`https://deusnotam.github.io/D3US/d3us.js`); // Настройщик тильды
-
-    function loadScript(src) {
-      const script = document.createElement("script");
-      script.src = src;
-      document.head.appendChild(script);
-    }
-
-    window.AbrosInj = true;
-  })();
-}
+      wrapper.style.position = "absolute";
+      wrapper.style.bottom = randomBottom;
+      wrapper.style.right = randomRight;
+    });
+  }
+});
