6fb5d416fd5e53c7bc2d5081772b04f61865cdee
abrosdaniel <danyaabros@gmail.com>
2025-07-04 22:27:35 +0200

    UPD

--- tilab/services/debug.js
+++ tilab/services/debug.js
@@ -1,2 +1,197 @@
-const Console = `
-<div><div>`;
+(function (window) {
+  const html = `
+<div
+    class="tilab"
+    style="
+      display: block;
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      margin: 0 !important;
+      padding: 0 !important;
+      font-size: 16px !important;
+      --tsqd-panel-height: 536px;
+      --tsqd-panel-width: 500px;
+      --tsqd-font-size: 16px;
+    "
+  >
+    <style>
+      .tilab * {
+        box-sizing: border-box !important;
+        text-transform: none !important;
+        font-family: ui-sans-serif, Inter, system-ui, sans-serif, sans-serif;
+        color: #d0d5dd;
+        text-align: left;
+      }
+      .tilab-frame {
+        width: 100vw;
+        height: 40em;
+        position: relative;
+      }
+      .tilab aside {
+        width: 100%;
+        height: 100%;
+        flex-direction: row;
+        background-color: #0b0d10;
+        max-height: 90%;
+        min-height: calc(var(--tsqd-font-size) * 3.5);
+        position: fixed;
+        z-index: 9999;
+        display: flex;
+        gap: calc(var(--tsqd-font-size) * 0.125);
+      }
+      .tilab-drag-handle {
+        position: absolute;
+        transition: background-color 0.125s ease;
+        z-index: 4;
+        top: 0;
+        width: 100%;
+        height: 3px;
+        cursor: ns-resize;
+      }
+      .tilab-drag-handle:hover {
+        background-color: #9b8afbe5;
+      }
+      .tilab-close {
+        position: absolute;
+        cursor: pointer;
+        z-index: 5;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        outline: none;
+        background-color: #191c24;
+        top: 0;
+        right: calc(var(--tsqd-font-size) * 0.5);
+        transform: translate(0, -100%);
+        border-right: #394056 1px solid;
+        border-left: #394056 1px solid;
+        border-top: #394056 1px solid;
+        border-bottom: none;
+        border-radius: calc(var(--tsqd-font-size) * 0.25)
+          calc(var(--tsqd-font-size) * 0.25) 0px 0px;
+        padding: calc(var(--tsqd-font-size) * 0.25)
+          calc(var(--tsqd-font-size) * 0.375)
+          calc(var(--tsqd-font-size) * 0.125)
+          calc(var(--tsqd-font-size) * 0.375);
+      }
+      .tilab-close:hover {
+        background-color: #292e3d;
+      }
+      .tilab-close::after {
+        content: " ";
+        position: absolute;
+        top: 100%;
+        left: -calc(var(--tsqd-font-size) * 0.625);
+        height: calc(var(--tsqd-font-size) * 0.375);
+        width: calc(100% + calc(var(--tsqd-font-size) * 1.25));
+      }
+      .tilab-close svg {
+        color: #98a2b3;
+        width: calc(var(--tsqd-font-size) * 0.5);
+        height: calc(var(--tsqd-font-size) * 0.5);
+      }
+      .tilab-section {
+        flex: 1 1 700px;
+        background-color: #191c24;
+        display: flex;
+        flex-direction: column;
+      }
+      .tilab-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: calc(var(--tsqd-font-size) * 0.5)
+          calc(var(--tsqd-font-size) * 0.625);
+        gap: calc(var(--tsqd-font-size) * 0.625);
+        border-bottom: #292e3d 1px solid;
+      }
+      .tilab-logo {
+        display: flex;
+        flex-direction: column;
+        background-color: transparent;
+        border: none;
+        gap: calc(var(--tsqd-font-size) 16px * 0.125);
+        padding: 0px;
+      }
+      .tilab-logo-tilab {
+        font-size: var(--tsqd-font-size);
+        font-weight: 700;
+        line-height: calc(var(--tsqd-font-size) * 1);
+        white-space: nowrap;
+        color: #d0d5dd;
+      }
+      .tilab-logo-desc {
+        font-weight: 600;
+        font-size: calc(var(--tsqd-font-size) * 0.75);
+        background: linear-gradient(to right, #dd524b, #e9a03b);
+        background-clip: text;
+        -webkit-background-clip: text;
+        line-height: 1;
+        -webkit-text-fill-color: transparent;
+        white-space: nowrap;
+      }
+      .tilab-frame[data-state="false"] {
+        display: none;
+      }
+    </style>
+    <div class="tilab-frame" data-state="true">
+      <aside aria-label="TiLab debug">
+        <div class="tilab-drag-handle"></div>
+        <button aria-label="Close TiLab debug" class="tilab-close">
+          <svg
+            width="10"
+            height="6"
+            viewBox="0 0 10 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 1L5 5L9 1"
+              stroke="currentColor"
+              stroke-width="1.66667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </button>
+        <div class="tilab-section">
+          <div class="tilab-header">
+            <div class="tilab-logo">
+              <span class="tilab-logo-tilab">TILAB</span>
+              <span class="tilab-logo-desc">Версия: 1.0.0</span>
+            </div>
+            <div class="tilab-status"></div>
+          </div>
+        </div>
+        <div class="tilab-section"></div>
+      </aside>
+      <button></button>
+    </div>
+  </div>`;
+
+  function switchState() {
+    const frame = document.querySelector(".tilab-frame");
+    if (frame) {
+      const currentState = frame.getAttribute("data-state") === "true";
+      frame.setAttribute("data-state", (!currentState).toString());
+    }
+  }
+
+  function initializeDebugPanel() {
+    // const container = document.createElement("div");
+    // container.innerHTML = html;
+    document.body.appendChild(html);
+
+    const closeButton = document.getElementById("tilab-close");
+    if (closeButton) {
+      closeButton.addEventListener("click", switchState);
+    }
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initializeDebugPanel);
+  } else {
+    initializeDebugPanel();
+  }
+})(window);
